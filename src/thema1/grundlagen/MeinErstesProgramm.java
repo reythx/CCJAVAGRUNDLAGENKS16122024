c855a73b5c11184723950044772079d707c24fc0
Kevin Schmidt <KSCH368@more2learn.de>
2024-12-16 12:26:52 +0100

    Add Rechner Programme

---
+++ src/thema1/grundlagen/MeinErstesProgramm.java
@@ -0,0 +1,11 @@
+package thema1.grundlagen;
+
+public class MeinErstesProgramm {
+
+	public static void main(String[] args) {
+		int zahl1 = 10;
+		int zahl2 = 20;
+		
+	}
+
+}
---
+++ src/thema1/grundlagen/RechnerProgrammSchmidt.java
@@ -0,0 +1,22 @@
+package thema1.grundlagen;
+
+public class RechnerProgrammSchmidt {
+
+	public static void main(String[] args) {
+		
+		int zahl1 = 10;
+		int zahl2 = 20;
+		int ergebnis = zahl1+zahl2;
+		
+		System.out.println("Addition von " + zahl1 + " und " + zahl2 + " ergibt = " + ergebnis);
+		
+		int zahl3 = 3;
+		int zahl4 = 13;
+		int ergebnis2 = zahl3 + zahl4;
+		
+		System.out.println("Addition von " + zahl3 + " und "+zahl4 + " ist " + ergebnis2);
+		
+	}
+	
+
+}
