f0932f5ae2971ea0f973a91d4a77cc1f205fa173
Kevin Schmidt <KSCH368@more2learn.de>
2024-12-16 12:39:37 +0100

    Change folder name

--- src/thema1/grundlagen/RechnerOO.java
+++ src/thema1/grundlagen/RechnerOO.java
@@ -1,7 +1,14 @@
 package thema1.grundlagen;
 
 public class RechnerOO {
-
+	
+	public static void main(String[] args) {
+		additionUndAusgabe(23, 24);
+		substraktionUndAusgabe(2,5);
+		multiplizierenUndAusgeben(3, 7);
+		dividierenUndAusgeben(3, 6);
+	}
+	
 	public static void additionUndAusgabe(int z1, int z2) {
 		int ergebnis = z1 + z2;
 		System.out.println("Addition von " + z1 + " und " + z2 + " ergibt = " + ergebnis);
@@ -30,12 +37,6 @@ public class RechnerOO {
 		
 	}
 	
-	public static void main(String[] args) {
-		additionUndAusgabe(23, 24);
-		substraktionUndAusgabe(2,5);
-		multiplizierenUndAusgeben(3, 7);
-		dividierenUndAusgeben(3, 6);
-	}
 }	
 	
 
